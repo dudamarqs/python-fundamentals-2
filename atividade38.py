dc44a1853057e55a61f065e361fb409a366c08f7
dudamarqs <eduardamarquesnoleto@gmail.com>
2024-07-03 19:33:37 -0300

    Code to compare two numbers and display which number is bigger

---
+++ atividade38.py
@@ -0,0 +1,9 @@
+number1 = int(input('Digite um número: '))
+number2 = int(input('Digite outro número: '))
+
+if number1 > number2:
+    print('\nO 1º número é maior.')
+elif number2 > number1:
+    print('\n O 2º número é maior.')
+else:
+    print('\nOs números são iguais.')
