ed45456c4644546384e6398af66bb7376235ad27
dudamarqs <eduardamarquesnoleto@gmail.com>
2024-07-04 19:00:56 -0300

    Program that calculates an athlete's age and shows their category

---
+++ atividade41.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+year_birth = int(input('Em que ano você nasceu? '))
+year = date.today().year
+age = year - year_birth
+print('O atleta tem {} anos.'.format(age))
+
+if age < 10:
+    print('Categoria: MIRIM.')
+elif 9 < age < 15:
+    print('Categoria: INFANTIL.')
+elif 14 < age < 20:
+    print('Categoria: JÚNIOR.')
+elif age == 20:
+    print('Categoria: SÊNIOR.')
+else:
+    print('Categoria: MASTER.')
