a72688c3a2c95ecdb3b1858e0637fd34f108ac8c
dudamarqs <eduardamarquesnoleto@gmail.com>
2024-07-05 21:07:37 -0300

    Code to check if a number is prime or not

---
+++ atividade53.py
@@ -0,0 +1,10 @@
+sentence = str(input('Digite uma frase: ')).strip().upper()
+words = sentence.split()
+joinedWords = ''.join(words)
+inverse = ''
+for c in range(len(joinedWords) - 1, -1, -1):
+    inverse += joinedWords[c]
+if inverse == joinedWords:
+    print('A frase digitada é um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo')
